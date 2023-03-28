8c8e8c15099bd59dd5d0404204be8b36fbbcded1
rahulTiwari021 <31885581+rahulTiwari021@users.noreply.github.com>
2023-03-28 19:09:36 +0530

    Update router.js

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const { request } = require('graphql-request');
 const { auth } = require('./auth-middleware');
 const routes = express.Router();
 
-const secretKey = process.env.SECRET_KEY || 'mysecretkey';
+const secretKey = process.env.SECRET_KEY || 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugd\nUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQs\nHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5D\no2kQ+X5xK9cipRgEKwIDAQAB';
 const API_URL = 'http://localhost:8080/v1/graphql';
   
 
@@ -14,14 +14,23 @@ routes.get('/', (req, res) => {
 
 routes.post('/login', (req, res) => {
     // TODO implement user verification. Keeping static now as there is no unique email or mobile and password to login and verify
-    const token = jwt.sign({ username: 'rahul' }, secretKey);
+    const token = jwt.sign({
+       "https://hasura.io/jwt/claims": {
+           "x-hasura-default-role": "role1",
+           "x-hasura-allowed-roles": ["role1"]
+        }
+    }, secretKey);
     res.send({ token });
+});
+
+routes.post('/verify-jwt', auth, (req, res) => {
+    res.send('Success');
 });
 
 routes.post('/get-users', auth,  async (req, res) => {
     const { page_size, page_number } = req.body;
     const limit = page_size;
-    const offset = page_number * page_size;
+    const offset = page_number * page_size; 
 
     try {
         const query = `query {
@@ -29,7 +38,6 @@ routes.post('/get-users', auth,  async (req, res) => {
                 id
                 first_name
                 last_name
-                gender
             }
         }`;
 
