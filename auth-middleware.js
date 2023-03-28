7567635963efcc840d9728f02ead081c3951bd45
rahulTiwari021 <31885581+rahulTiwari021@users.noreply.github.com>
2023-03-28 19:08:18 +0530

    Update auth-middleware.js

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,6 +1,6 @@
 const { expressjwt: jwt } = require('express-jwt');
 
-const secretKey = process.env.SECRET_KEY || 'mysecretkey';
+const secretKey = process.env.SECRET_KEY || 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugd\nUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQs\nHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5D\no2kQ+X5xK9cipRgEKwIDAQAB';
 
 const auth = jwt({
     secret: secretKey,
